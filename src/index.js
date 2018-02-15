9d3d886e98226605332a8303fdbdccc2cef30480
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-02-15 15:25:38 +0900

    Modify index.js for roulette

--- src/index.js
+++ src/index.js
@@ -1,8 +1,58 @@
+// https://codezine.jp/article/detail/9928
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Roulette extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 1,
+      isStart: false,
+    };
+    this.toggleButton = this.toggleButton.bind(this);
+  }
+
+  shuffle() {
+    let rand = Math.floor(Math.random() * 30) + 1;
+    this.setState({number: rand});
+  }
+
+  startRoulette(){
+    this.interval = setInterval(() => this.shuffle(), 50);
+  }
+
+  stopRoulette() {
+    clearInterval(this.interval);
+  }
+
+  toggleButton() {
+    if (this.state.isStart) {
+      this.stopRoulette();
+    } else {
+      this.startRoulette();
+    }
+
+    this.setState((prevState) => ({
+      isStart: !prevState.isStart
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <p>number: {this.state.number}</p>
+        <button onClick={this.toggleButton}>
+          {this.state.isStart ? 'Stop' : 'Start'}
+        </button>
+      </div>
+    );
+  }
+}
+
+const element = <Roulette />;
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
